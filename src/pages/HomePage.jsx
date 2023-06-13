765802c236e6351b6d3fb6b1406f2c8ec88083b1
Palakmidha <78557067+Palakmidha@users.noreply.github.com>
2023-06-13 23:18:20 +0530

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import Contributions from "../components/Contributions";
+import CardSection from "../components/CardSection";
+import MentorSection from "../components/MentorSection";
+import ResourcesSection from "../components/ResourcesSection";
+import Footer from "../components/Footer";
+import ProjectHelpSection from "../components/ProjectHelpSection";
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <HeroSection />
+      <CardSection />
+      <MentorSection />
+      <ResourcesSection />
+      <ProjectHelpSection/>
+      <Contributions />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
+
